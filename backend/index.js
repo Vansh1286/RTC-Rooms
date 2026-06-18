b39a0ccc00cc88daab239448e2143fbe45d694bf
Vansh1286 <vanshrajput931@gmail.com>
2026-06-19 01:16:08 +0530

    feat: add Dockerfile for multi-stage build and .dockerignore file

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 // const express = require('express');
 import express from 'express';
 import "dotenv/config";
+import fs from 'fs';
+import path from'path';
 
 import {clerkMiddleware} from "@clerk/express";
 
@@ -14,6 +16,8 @@ const app = express();
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
+const publicDir = path.join(process.cwd(), 'public');
+
 app.use(express.json());
 app.use(cors({origin: FRONTEND_URL, credentials: true}));
 app.use(clerkMiddleware());
@@ -21,6 +25,17 @@ app.use(clerkMiddleware());
 app.get("/health", (req, res) => {
   res.status(200).json({ ok: true});
 });
+
+//if the public directory exists, serve the static files from it
+//this is where the frontend will be built and served from
+//this is only for production, in development the frontend will be served from the Vite dev server
+if(fs.existsSync(publicDir)){
+  app.use(express.static(publicDir));
+
+  app.get('/{*any}', (req, res, next) => {
+    res.sendFile(path.join(publicDir, 'index.html'), (err) => next(err));
+  });
+}
 
  
 
